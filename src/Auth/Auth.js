393caf46b27d18ae1208f8b6041b1ddc5e2a151c
AleksandrBorkun <alexandr.borkun1992@gmail.com>
2019-08-26 14:26:21 +0200

    add silence log in, refactor private routes

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -1,5 +1,14 @@
 import auth0 from 'auth0-js';
 
+const REDIRECT_ON_LOGIN = "redirect_on_login"
+
+// eslint-disable-next-line
+let _idToken = null;
+let _accessToken = null;
+let _scopes = null;
+let _expiresAt = null;
+
+
 export default class Auth {
     constructor(history) {
         this.userProfile = null;
@@ -15,6 +24,7 @@ export default class Auth {
         })
     }
     login = () => {
+        localStorage.setItem(REDIRECT_ON_LOGIN, JSON.stringify(this.history.location));
         this.auth0.authorize();
     }
 
@@ -22,36 +32,34 @@ export default class Auth {
         this.auth0.parseHash((err, authResult) => {
             if (authResult && authResult.accessToken && authResult.idToken) {
                 this.setSession(authResult);
-                this.history.push("/");
+                const redirectLocation = 
+                localStorage.getItem(REDIRECT_ON_LOGIN) === "undefined" 
+                ? "/" 
+                : JSON.parse(localStorage.getItem(REDIRECT_ON_LOGIN));
+                console.log(redirectLocation)
+                this.history.push(redirectLocation);
             } else if (err) {
                 this.history.push("/")
                 alert(`Error ${err.error}. Check Console for the details`);
                 console.log(err);
             }
+            localStorage.removeItem(REDIRECT_ON_LOGIN);
         })
     }
 
     setSession = authResult => {
-        const expiresAt = JSON.stringify(authResult.expiresIn * 1000 + new Date().getTime());
-
-        const scopes = authResult.scope || this.requestedScopes || "";
-        localStorage.setItem("access_token", authResult.accessToken);
-        localStorage.setItem("id_token", authResult.idToken);
-        localStorage.setItem("expires_at", expiresAt);
-        localStorage.setItem("scopes", JSON.stringify(scopes));
+        _expiresAt = authResult.expiresIn * 1000 + new Date().getTime();
+        _scopes = authResult.scope || this.requestedScopes || "";
+        _accessToken = authResult.accessToken;
+        _idToken =  authResult.idToken;
+        this.scheduleTokenRenewal();
     }
 
     isAuthenticated() {
-        const expiresAt = JSON.parse(localStorage.getItem("expires_at"));
-        return new Date().getDate() < expiresAt;
+        return new Date().getDate() < _expiresAt;
     }
 
     logout = () => {
-        localStorage.removeItem("access_token");
-        localStorage.removeItem("id_token");
-        localStorage.removeItem("expires_at");
-        localStorage.removeItem("scopes");
-        this.userProfile = null;
         this.auth0.logout({
             clientID: process.env.REACT_APP_AUTH0_CLIENTID,
             returnTo: "http://localhost:3000"
@@ -59,11 +67,10 @@ export default class Auth {
     }
 
     getAccessToken = () => {
-        const accessToken = localStorage.getItem("access_token");
-        if (!accessToken) {
+        if (!_accessToken) {
             throw new Error("No access token found.");
         }
-        return accessToken;
+        return _accessToken;
     }
 
     getProfile = cb => {
@@ -75,9 +82,23 @@ export default class Auth {
     }
 
     userHasScopes(scopes) {
-        const grantedScopes = (
-            JSON.parse(localStorage.getItem("scopes")) || ""
-        ).split(" ");
+        const grantedScopes = (_scopes || "").split(" ");
         return scopes.every(scope => grantedScopes.includes(scope))
+    }
+
+    renewToken(cb){
+        this.auth0.checkSession({}, (err, result)=>{
+            if(err){
+                console.log(`Error: ${err.error} - ${err.error_description}.`);
+            }else{
+                this.setSession(result);
+            }
+            if(cb) cb(err, result);
+        })
+    }
+
+    scheduleTokenRenewal(){
+        const delay = _expiresAt - Date().now();
+        if (delay> 0) setTimeout(() => this.renewToken(), delay);
     }
 }
